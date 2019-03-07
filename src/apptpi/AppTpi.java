00c778342ccc940204dda6410556f1aa05f21611
jongarcia <>
2019-03-07 10:10:59 -0600

    Java App Commit 01

---
+++ src/apptpi/AppTpi.java
@@ -0,0 +1,16 @@
+package apptpi;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jonahdz
+ */
+public class AppTpi {
+
+    public static void main(String[] args) {
+    
+        JOptionPane.showMessageDialog(null,"Hola Mundo");
+        
+    }    
+}
