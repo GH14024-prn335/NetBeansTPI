9a955fdfba7abca0faa2ccd99c0b19195882266f
astjek <>
2019-03-07 10:41:48 -0600

    Java App Commit 02

--- src/apptpi/AppTpi.java
+++ src/apptpi/AppTpi.java
@@ -4,13 +4,15 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author jonahdz
+ * @author jonahdz and astjek
  */
 public class AppTpi {
 
     public static void main(String[] args) {
     
         JOptionPane.showMessageDialog(null,"Hola Mundo");
+        int nueva= Integer.parseInt(JOptionPane.showInputDialog("Ingresa Algo"));
+        JOptionPane.showMessageDialog(null,nueva);
         
     }    
 }
